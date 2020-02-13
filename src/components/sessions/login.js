aeffc231e286338add4b25acae90f81e6c7ae6b9
Michael-Xie <xiemicha@gmail.com>
2020-02-13 18:53:28 -0500

    Update user state and have it also show up on next to logo

--- src/components/sessions/login.js
+++ src/components/sessions/login.js
@@ -3,43 +3,43 @@ import styled from "styled-components"
 //import handleRegister from "../../models/register"
 import axios from "axios"
 
-const validateLogin = (userName, password, users) => {
-  const findUser = users.filter((user) => {
-    return user.user_name === userName && user.password === password;
-  })
-  console.log(findUser);
-  return findUser.length > 0
-}
 
-const handleLogin = (event) => {
-  event.preventDefault()
-  const user = event.target.username.value
-  const password = event.target.password.value
-  //[TODO] validate username and password
-
-  //
-  axios.get("http://localhost:8001/api/users", {
-    user_name: user,
-    password: password,
-  })
-    .then(res => {
-      console.log(res);
-      console.log("login status", validateLogin(user, password, res.data));
-      console.log("getting info from DB");
-      if (validateLogin(user, password, users)) {
-        dispatch({
-          type: SET_USER,
-          value: {
-            user: user
-          }
-        });
-  
-      }
+export default function Login({dispatch}) {
+  const validateLogin = (userName, password, users) => {
+    const findUser = users.filter((user) => {
+      return user.user_name === userName && user.password === password;
     })
-    .catch(err => console.log(err))
-}
-
-export default function Login({ reducer }) {
+    return findUser.length > 0
+  }
+  
+  const handleLogin = (event) => {
+    event.preventDefault()
+    const user = event.target.username.value
+    const password = event.target.password.value
+    //[TODO] validate username and password
+  
+    //
+    axios.get("http://localhost:8001/api/users", {
+      user_name: user,
+      password: password,
+    })
+      .then(res => {
+        console.log(res);
+        console.log("login status", validateLogin(user, password, res.data));
+        console.log("getting info from DB");
+        if (validateLogin(user, password, res.data)) {
+          dispatch({
+            type: "SET_USER",
+            value: {
+              username: user
+            }
+          });
+    
+        }
+      })
+      .catch(err => console.log(err))
+  }
+  
   return (
     <>
       <h2>Login</h2>
