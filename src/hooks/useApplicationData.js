563a1d484c2f2c1578ec1b67a7edc81d068d185d
Anthony Pisani <antoineincognito@hotmail.com>
2020-02-14 16:14:49 -0500

    fixed websockets updating bug

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -9,24 +9,36 @@ const useApplicationData = () => {
   })
 
   useEffect(() => {
-    axios.get("http://localhost:8001/api/games")
+    axios.get("http://localhost:8001/api/games/0")
       .then(res => {
-        console.log("called")
         const games = res.data.sort((a, b) => b.date - a.date)
         dispatch({type: "SET_GAMES", games: games})
       })
   }, [])
-  
-  let i = 0
-  setInterval(() => {
-    axios.get("http://localhost:8001/api/games")
-      .then(res => {
-        i++
-        console.log(`Games Updated #${i}`)
-        const games = res.data.sort((a, b) => b.date - a.date)
-        dispatch(() => ({type: "SET_GAMES", games: games}))
-      })
-  }, 30000)
+
+  // useEffect to establish a websocket connection on the client side.
+  useEffect(() => {
+    const sock = new WebSocket('ws://localhost:8001')
+
+    sock.addEventListener("open", function() {
+      console.log('Connection established...');
+    })
+
+    sock.addEventListener("message", function(msg) {
+      console.log("basket-ball api called to update scores")
+      const event = JSON.parse(msg.data)
+      if (event.type === "SET_GAMES") {
+        return dispatch({...event})
+      }
+    })
+
+    sock.addEventListener('close', function() {
+      console.log("Connection closed");
+    })
+
+    return () => { sock.close() };
+  }, [])
+
 
   return {state, dispatch}
 }
