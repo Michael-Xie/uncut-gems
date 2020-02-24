99e6023e2bfd19492ef8d3af94a45a3187c09987
Michael-Xie <xiemicha@gmail.com>
2020-02-24 13:16:28 -0500

    Add netlify config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import Application from './components/Application';
 import * as serviceWorker from './serviceWorker';
 
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 ReactDOM.render(<Application />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
